8071f756ecc50d915626df1adc6b8dc72887e7a1
hungpt2004 <hungptfpt2004@gmail.com>
2023-08-27 13:06:38 +0700

    nop bai week 4

--- week_four/Exercise3_week4.dart
+++ week_four/Exercise3_week4.dart
@@ -1,6 +1,19 @@
-
+import 'dart:math';
+class Shape{
+  double calculateArea(){
+    return 0;
+  }
+}
+class Circle extends Shape{
+  late double radius;
+  double calculateArea(){
+    return radius*radius*pi;
+  }
+}
 
 
 void main(List<String> args) {
-  
+  var c = Circle();
+  c.radius=4;
+  print(c.calculateArea().toStringAsFixed(2));
 }
