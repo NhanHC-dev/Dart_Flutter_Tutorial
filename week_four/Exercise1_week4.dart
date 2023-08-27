c12d509dbb3ea66c47bffd2865903dbf65ef44cf
hungpt2004 <hungptfpt2004@gmail.com>
2023-08-27 12:31:41 +0700

    Ex1_week4

---
+++ week_four/Exercise1_week4.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'dart:math';
+class shape{
+  late String color;
+  void calculateArea(){
+    print("shape area is calculated");
+  }
+}
+void main(List<String> arguments) {
+//Create a class named Shape with a color property and a calculateArea() method. The calculateArea() method should print "Shape area is calculated
+var triangle = shape();
+triangle.color="red";
+triangle.calculateArea();
+}
